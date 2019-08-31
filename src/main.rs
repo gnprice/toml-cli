ed159bda7c7aab4ea3882009ec6673dead20359c
Greg Price <gnprice@gmail.com>
2019-08-30 21:19:11 -0700

    Quick demo of parsing a TOML file.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use toml::{Value};
+
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let path = "Cargo.toml";
+    let data = fs::read(path)?;
+    let whole: Value = toml::from_slice(&data).unwrap();
+    println!("{}", whole.get("package").unwrap().get("authors").unwrap()[0]);
+    Ok(())
 }
