c9121e5df601c09ff7b792718850f8bb96b30e3e
Greg Price <gnprice@gmail.com>
2019-08-30 21:30:49 -0700

    Quick demo with toml_edit crate instead.
    This looks like it'll do the job!
    
    The `toml` crate doesn't seek to preserve formatting, which makes it
    not so much the tool for this particular job.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::fs;
-use toml::{Value};
+use std::str;
+use toml_edit::{Document, value};
 
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let path = "Cargo.toml";
     let data = fs::read(path)?;
-    let whole: Value = toml::from_slice(&data).unwrap();
-    println!("{}", whole.get("package").unwrap().get("authors").unwrap()[0]);
+    let data = str::from_utf8(&data)?;
+    let mut doc = data.parse::<Document>()?;
+    println!("{:#?}", doc["package"]["authors"][0]);
+    doc["package"]["foo"] = value("bar");
+    println!("{}", doc.to_string());
     Ok(())
 }
