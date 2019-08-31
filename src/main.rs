f6be2ac9fc6b1336acebcf0c7fb40a16bfc8e486
Greg Price <gnprice@gmail.com>
2019-08-31 16:08:10 -0700

    Add some first crude CLI parsing, using structopt (and clap).
    Googled [rust cli]; that took me to
      https://www.rust-lang.org/what/cli
    which sent me to
      https://rust-lang-nursery.github.io/cli-wg/
    which recommended this set of libraries.
    Seem like a reasonable design; trying them out.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 use std::fs;
+use std::path::PathBuf;
 use std::str;
+use structopt::StructOpt;
 use toml_edit::{Document, value};
 
+#[derive(StructOpt)]
+enum Args {
+    Get {
+        #[structopt(parse(from_os_str))]
+        path: PathBuf,
+        query: String,
+    },
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
-    let path = "Cargo.toml";
+    let args = Args::from_args();
+    match args {
+        Args::Get { path, query } => get(path, query),
+    }
+}
+
+fn get(path: PathBuf, query: String) -> Result<(), Box<dyn std::error::Error>> {
     let data = fs::read(path)?;
     let data = str::from_utf8(&data)?;
-    let mut doc = data.parse::<Document>()?;
+    let doc = data.parse::<Document>()?;
+
     println!("{:#?}", doc["package"]["authors"][0]);
+
+    /*
     doc["package"]["foo"] = value("bar");
     println!("{}", doc.to_string());
+    */
     Ok(())
 }
