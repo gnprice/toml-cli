45e52b9fce22401af9674470b380f55973015281
Greg Price <gnprice@gmail.com>
2019-08-31 16:37:39 -0700

    Add crude indexing into tables.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::fs;
 use std::path::PathBuf;
 use std::str;
+
+use regex::Regex;
 use structopt::StructOpt;
 use toml_edit::{Document, value};
 
@@ -16,16 +18,23 @@ enum Args {
 fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let args = Args::from_args();
     match args {
-        Args::Get { path, query } => get(path, query),
+        Args::Get { path, query } => get(path, &query),
     }
 }
 
-fn get(path: PathBuf, query: String) -> Result<(), Box<dyn std::error::Error>> {
+fn get(path: PathBuf, mut query: &str) -> Result<(), Box<dyn std::error::Error>> {
     let data = fs::read(path)?;
     let data = str::from_utf8(&data)?;
     let doc = data.parse::<Document>()?;
+    let mut item = &doc.root;
 
-    println!("{:#?}", doc["package"]["authors"][0]);
+    let re_byname = Regex::new(r"\A\.(\w+)").unwrap();
+    while let Some(cap) = re_byname.captures(&query) {
+        item = &item[cap.get(1).unwrap().as_str()];
+        query = &query[cap.get(0).unwrap().end()..];
+    }
+    // TODO: [0]
+    println!("{:#?}", item);
 
     /*
     doc["package"]["foo"] = value("bar");
