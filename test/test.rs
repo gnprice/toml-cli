8fb8cc5233d43532abb634c596fd4050eb8737be
Chevdor <chevdor@gmail.com>
2022-10-04 12:55:22 +0200

    Bump the toml_edit dep to 0.6

--- test/test.rs
+++ test/test.rs
@@ -7,6 +7,7 @@ use std::str;
 
 use tempfile::TempDir;
 
+<<<<<<< HEAD
 macro_rules! tomltest {
     ($name:ident, $fun:expr) => {
         #[test]
@@ -234,4 +235,60 @@ fn check_eq(expected: &str, actual: &str) {
 {}~~~
 ", expected, actual);
     }
+||||||| parent of 8c94d56 (Bump the toml_edit dep to 0.6)
+		#[test]
+		fn it_shows_help() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_BIN_EXE_toml")).unwrap();
+			let assert = cmd.arg("--help").assert();
+			assert.success().code(0);
+		}
+	}
+
+	#[cfg(test)]
+	mod get {
+		use assert_cmd::Command;
+
+		#[test]
+		fn it_gets_simple() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_BIN_EXE_toml")).unwrap();
+			let assert = cmd.args(&["get", "Cargo.toml", "version"]).assert();
+			assert.success().code(0);
+		}
+
+		#[test]
+		#[ignore = "Requires a update of the toml_edit dependency at least"]
+		fn it_gets_dotted() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_BIN_EXE_toml")).unwrap();
+			let assert = cmd.args(&["get", "test/demo.toml", "version"]).assert();
+			assert.success().code(0);
+		}
+	}
+=======
+		#[test]
+		fn it_shows_help() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_BIN_EXE_toml")).unwrap();
+			let assert = cmd.arg("--help").assert();
+			assert.success().code(0);
+		}
+	}
+
+	#[cfg(test)]
+	mod get {
+		use assert_cmd::Command;
+
+		#[test]
+		fn it_gets_simple() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_BIN_EXE_toml")).unwrap();
+			let assert = cmd.args(&["get", "Cargo.toml", "version"]).assert();
+			assert.success().code(0);
+		}
+
+		#[test]
+		fn it_gets_dotted() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_BIN_EXE_toml")).unwrap();
+			let assert = cmd.args(&["get", "test/demo.toml", "version"]).assert();
+			assert.success().code(0);
+		}
+	}
+>>>>>>> 8c94d56 (Bump the toml_edit dep to 0.6)
 }
