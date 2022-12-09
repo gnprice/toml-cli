0a3a1acc785eb5e2450374a49435dcff0f01b4f1
Greg Price <gnprice@gmail.com>
2022-12-08 22:20:23 -0800

    tools: Use terminal colors in release instructions
    Well, bold, anyway.

--- tools/lib.bash
+++ tools/lib.bash
@@ -6,3 +6,23 @@ die() {
 get_version() {
     cargo run -q -- get Cargo.toml package.version --raw
 }
+
+# Set variables for color codes if color appropriate, or empty if not.
+#
+# Color is deemed appropriate just if stderr is a terminal.
+#
+# Variables set: reset, bold
+prepare_colors() {
+    local should_color=
+    if [ -t 2 ]; then
+        should_color=yes
+    fi
+
+    reset=
+    bold=
+    if [ -n "${should_color}" ]; then
+        reset=$'\033'[0m
+        bold=$'\033'[1m
+    fi
+    : "${reset}" "${bold}" # dummy use, to reassure shellcheck
+}
