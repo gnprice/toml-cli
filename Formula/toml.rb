8f37791c4f33ddfd04c1a162030be77621a0654d
TeraBot <chevdor@users.noreply.github.com>
2023-04-21 15:03:22 +0000

    build: new homebrew formula for v0.2.4

--- Formula/toml.rb
+++ Formula/toml.rb
@@ -1,9 +1,9 @@
 class Toml < Formula
   desc "A command line utility written in Rust download, inspect and compare Substrate based chains WASM Runtimes"
   homepage "https://github.com/chevdor/toml-cli"
-  url "https://github.com/chevdor/toml-cli/releases/download/v0.2.2/toml_macos_v0.2.2.tar.gz"
-  sha256 "daa2e7c8396e6bd00777f89e74197efb4e2e34b5bbae450cd98569fc7141cfd4"
-  version "0.2.2"
+  url "https://github.com/chevdor/toml-cli/releases/download/v0.2.4/toml_macos_v0.2.4.tar.gz"
+  sha256 "7cf5971646fda4e8edfd3723d05ff3d030e3d945dca72c32e1d2fc1f49529bb5"
+  version "0.2.4"
 
   def install
     bin.install "toml"
